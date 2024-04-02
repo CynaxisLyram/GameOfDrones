25d6208aec391f670c8cf2d7c63571afaaf5b074
Lyram <julien.monteil81@gmail.com>
2024-04-02 11:30:40 +0200

    Ajout du login

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$login = $_POST['login'];
+$pwd = $_POST['password'];
+if($login == "Peter" && $pwd == "Parker"){
+    $_SESSION['login'] = $login;
+    echo "Bienvenue ".$login;
+}else{
+    echo "Échec GROSSE MERDE!";
+}
